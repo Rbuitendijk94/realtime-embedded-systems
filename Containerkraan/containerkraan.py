9c33690aeccf2532eb659831a267739e43d11ad8
Ruben Buitendijk <ruben@lizard-apps.com>
2018-10-18 14:17:19 +0200

    naar locatie voor container
    alleen nog spreader

--- Containerkraan/containerkraan.py
+++ Containerkraan/containerkraan.py
@@ -25,10 +25,10 @@ class containerkraan (Module):
         self.knopGo= Marker()
         self.knopNood = Marker()
         self.oppakXcoordinaat = Register(8)
-        self.oppakYcoordinaat = Register()
-        self.oppakZcoordinaat = Register(2)
-        self.gewenstXcoordinaat= Register(2)
-        self.gewenstYcoordinaat= Register()
+        self.oppakYcoordinaat = Register(2)
+        self.oppakZcoordinaat = Register(1)
+        self.gewenstXcoordinaat= Register(3)
+        self.gewenstYcoordinaat= Register(3)
         self.gewenstZcoordinaat = Register()
 
     def sweep (self):
@@ -55,24 +55,21 @@ class containerkraan (Module):
         self.motorKraan.mark(True, self.knopGo and self.kraanPositie != self.oppakXcoordinaat and self.spreaderPositie ==5, False)
         self.richtingKraan.mark(True, self.kraanPositie < self.oppakXcoordinaat)
         self.richtingKraan.mark(False, self.kraanPositie > self.oppakXcoordinaat)
-        self.kraanPositie.set(self.kraanPositie +0.25 , self.motorKraan and self.richtingKraan,  self.kraanPositie)
-        self.kraanPositie.set(self.kraanPositie -0.25 , self.motorKraan and self.richtingKraan == False, self.kraanPositie)
+        self.kraanPositie.set(self.kraanPositie +0.25 , self.motorKraan and self.richtingKraan and self.oppakkenContainer,  self.kraanPositie)
+        self.kraanPositie.set(self.kraanPositie -0.25 , self.motorKraan and self.richtingKraan == False and self.oppakkenContainer, self.kraanPositie)
 
         #loopkat oppakken container
-        self.motorLoopkat.mark(True, self.knopGo and self.loopkatPositie != self.oppakYcoordinaat)
+        self.motorLoopkat.mark(True, self.knopGo and self.loopkatPositie != self.oppakYcoordinaat and self.oppakkenContainer,False)
         self.richtingLoopkat.mark(True, self.loopkatPositie < self.oppakYcoordinaat)
         self.richtingLoopkat.mark(False, self.loopkatPositie > self.oppakYcoordinaat)
         self.loopkatPositie.set(self.loopkatPositie +0.25 , self.motorLoopkat and self.richtingLoopkat,  self.loopkatPositie)
         self.loopkatPositie.set(self.loopkatPositie -0.25 , self.motorLoopkat and self.richtingLoopkat == False, self.loopkatPositie)
 
 
-
-
-        #open de klauw
-
+        #spreader
         self.motorSpreader.mark(True, self.knopGo and self.spreaderPositie != self.oppakZcoordinaat and self.oppakXcoordinaat == self.kraanPositie and self.oppakYcoordinaat == self.loopkatPositie)
         self.motorSpreader.mark(False, self.spreaderPositie == self.oppakZcoordinaat and self.gewichtAanwezig == False)
-        self.motorSpreader.mark(False ,self.oppakkenContainer== False and self.gewichtAanwezig)
+        self.motorSpreader.mark(False ,self.oppakkenContainer== False and self.gewichtAanwezig) #deze
         self.spreaderPositie.set(self.spreaderPositie -0.25 , self.motorSpreader and self.richtingSpreader == False and self.gewichtAanwezig == False,  self.spreaderPositie)
         self.klauwenOpen.mark(True,self.gewichtAanwezig == False and self.spreaderPositie != self.oppakZcoordinaat )
         self.richtingSpreader.mark(False,self.spreaderPositie > self.oppakZcoordinaat )
@@ -80,43 +77,28 @@ class containerkraan (Module):
         self.klauwenOpen.mark(False,self.knopGo and self.spreaderPositie == self.oppakZcoordinaat and self.oppakXcoordinaat ==  self.kraanPositie and self.loopkatPositie == self.oppakYcoordinaat)
         self.gewichtAanwezig.mark(True, self.spreaderPositie == self.oppakZcoordinaat and self.oppakXcoordinaat ==  self.kraanPositie and self.loopkatPositie == self.oppakYcoordinaat and self.klauwenOpen == False)
         self.spreaderPositie.set(self.spreaderPositie +0.25 , self.motorSpreader and self.richtingSpreader and self.gewichtAanwezig,  self.spreaderPositie)
-        self.oppakkenContainer.mark(False, self.spreaderPositie == 5, self.gewichtAanwezig)
-'''
-        self.klauwenOpen.mark(False,self.spreaderPositie == self.oppakZcoordinaat)
-        self.gewichtAanwezig.mark(True, self.spreaderPositie == self.oppakZcoordinaat and self.klauwenOpen)
-        self.spreaderPositie.set(self.spreaderPositie -0.25 , self.motorSpreader and self.richtingSpreader == False and self.klauwenOpen == False, self.spreaderPositie)
+        self.oppakkenContainer.mark(False, self.spreaderPositie == 5 and self.gewichtAanwezig)
 
-
-
-
-#-----------------------------------------------------------brengen naar nieuwe locatiee
 
         #kraan naar bestemming brengen
-       #self.motorKraan.mark(True, self.knopGo and (self.kraanPositie *0.99 > self.gewenstXcoordinaat or self.kraanPositie *1.01 < self.gewenstXcoordinaat ),False)
-        self.motorKraan.mark(True, self.knopGo and self.kraanPositie != self.gewenstXcoordinaat and self.spreaderPositie ==5, False)
+        self.motorKraan.mark(True, self.knopGo and self.kraanPositie != self.gewenstXcoordinaat and self.spreaderPositie ==5 and self.oppakkenContainer == False, False)
         self.richtingKraan.mark(True, self.kraanPositie < self.gewenstXcoordinaat)
         self.richtingKraan.mark(False, self.kraanPositie > self.gewenstXcoordinaat)
-        self.kraanPositie.set(self.kraanPositie +0.25 , self.motorKraan and self.richtingKraan,  self.kraanPositie)
-        self.kraanPositie.set(self.kraanPositie -0.25 , self.motorKraan and self.richtingKraan == False, self.kraanPositie)
+        self.kraanPositie.set(self.kraanPositie +0.25 , self.motorKraan and self.richtingKraan and self.oppakkenContainer == False,  self.kraanPositie)
+        self.kraanPositie.set(self.kraanPositie -0.25 , self.motorKraan and self.richtingKraan  == False and self.oppakkenContainer == False, self.kraanPositie)
 
         #loopkat naar bestemming
-        self.motorLoopkat.mark(True, self.knopGo and self.loopkatPositie != self.gewenstYcoordinaat)
+        self.motorLoopkat.mark(True, self.knopGo and self.loopkatPositie != self.gewenstYcoordinaat and  self.oppakkenContainer == False ,False)
         self.richtingLoopkat.mark(True, self.loopkatPositie < self.gewenstYcoordinaat)
         self.richtingLoopkat.mark(False, self.loopkatPositie > self.gewenstYcoordinaat)
         self.loopkatPositie.set(self.loopkatPositie +0.25 , self.motorLoopkat and self.richtingLoopkat,  self.loopkatPositie)
         self.loopkatPositie.set(self.loopkatPositie -0.25 , self.motorLoopkat and self.richtingLoopkat == False, self.loopkatPositie)
 
+#-----------------------------------------------------------brengen naar nieuwe locatiee
+
+
+
+
 
         #overige logica
-        self.knopGo.mark(False, self.kraanPositie == self.gewenstXcoordinaat and self.loopkatPositie == self.gewenstYcoordinaat and self.spreaderPositie == self.gewenstZcoordinaat)
-
-
-
-
-
-
-        •----- Fysica van hijskraan: er is een marker die aangeeft of er een last aan het mechanisme hangt. Als de last zakt en op de grond of een andere container komt te staan, wordt deze marker False.
-        • De motoren voor de wielen, de loopkat en het hijsmechanisme kunnen aan- en uitgeschakeld worden en van draairichting veranderd. Hierdoor beweegt de kraan.
-        • De gebruiker kan de co¨ordinaten van een op te pakken container (zoveelste in voor/achterrichting, zoveelste in links/rechtsrichting, zoveelste stapellaag) en die van een bestemming opgeven, en vervolgens op de “go”-knop drukken. De kraan beweegt dan zelf naar de opgegeven plek, pakt de container op, brengt deze naar de bestemming en laat hem weer zakken.
-
-        '''
+    #    self.knopGo.mark(False, self.kraanPositie == self.gewenstXcoordinaat and self.loopkatPositie == self.gewenstYcoordinaat and self.spreaderPositie == self.gewenstZcoordinaat)
